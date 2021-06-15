84a165d53cda6cff017b2583afa1b9847700da2d
Alexander Mazuruk <a.mazuruk@samsung.com>
2021-06-16 01:33:27 +0200

    Add copyright notices for pip's utils
    While all pip's code should've been submitted with license and copyright info,
    this commit resolves the issue for code mentioned in the commit msg title.
    
    Authors were gathered in following way:
    From pip:
    https://github.com/pypa/pip.git
    checked out on:
    20.1.1 tag (commit: 8356bc5161e0a9b3054f0e04e12d18feae4c2b46)
    from repository root, following command was run to get all authors of vcs
    package:
    $ for f in src/pip/_internal/utils/*; do echo $f; git shortlog -s -- $f; done
    
    Signed-off-by: Alexander Mazuruk <a.mazuruk@samsung.com>

--- src/fetchcode/vcs/pip/_internal/utils/misc.py
+++ src/fetchcode/vcs/pip/_internal/utils/misc.py
@@ -1,3 +1,32 @@
+# Copyright (c) 2008-2020 The pip developers:
+# A_Rog, Albert Tugushev, Anthony Sottile, Anubhav Patel, Benoit Pierre,
+# BorisZZZ, Chris Hunt, Chris Jerdonek, Christopher Hunt, Dave Jones,
+# Deepak Sharma, Donald Stufft, Dustin Ingram, Emil Burzo, Frost Ming,
+# GOTO Hayato, Jason R. Coombs, Jeremy Zafran, Krishna Oza, László Kiss Kollár,
+# Maxim Kurnikov, Nguyễn Gia Phong, Paul Moore, Pradyun Gedam, Stephan Erb,
+# Steve Dower, Stéphane Bidoul (ACSONE), Tom Forbes, Tzu-ping Chung,
+# Xavier Fernandez, Yuan Jing Vincent Yan, wim glenn
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
 from __future__ import absolute_import
 
 import contextlib
